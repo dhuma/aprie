3d1ba2e8d7d5cf3298ee6627c5dfb03a72ed0c61
dhuma <dhianican.channel@gmail.com>
2020-08-26 17:01:14 +0700

    Add files via upload

---
+++ relatedPostConfig.js
@@ -0,0 +1,10 @@
+ var labelArray = [
+          "Windows"
+      ];
+  var relatedPostConfig = {
+      homePage: "https://www.aprie.net/",
+      numPosts:6,
+      thumbnailSize:300,
+      widgetStyle:2,
+      callBack: function() {}
+  };
