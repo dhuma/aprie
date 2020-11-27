f76490b3e412183d28d21f76c6f2fa06850234fb
Apri.my.id <dhianican.channel@gmail.com>
2020-11-27 14:39:33 +0700

    Update aprie.my.id.js

--- aprie.my.id.js
+++ aprie.my.id.js
@@ -2,7 +2,7 @@
 (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/aprie/c55ebad1/jquery.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
 
 /*! Sharethis | sticky-share-buttons */
-(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/aprie/9661ad76/plugin.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+(function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/aprie/9661ad76/plugin.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
 
 /*! Main Menu */
 (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://cdn.statically.io/gh/dhuma/aprie/0b2060e8/main-menu.min.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
